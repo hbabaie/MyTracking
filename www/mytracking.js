56182bc8861d43e1bb447a7b2a09374ad3e986ff
hbabaie <hosein.babaie@gmail.com>
2014-03-01 16:02:40 -0800

    Update mytracking.js

--- www/mytracking.js
+++ www/mytracking.js
@@ -3,7 +3,7 @@ var mytracking = {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'GPSTracking', // mapped to our native Java class called "Calendar"
+            'MyTracking', // mapped to our native Java class called "Calendar"
             'StartListening', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "RemoteServer" : RemoteServer,
